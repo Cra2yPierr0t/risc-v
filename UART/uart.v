c6fbb9d80d2971094c9bf6c8654fb798aaf65e29
pirrot <uchiyama.kazuhide@gmail.com>
2019-10-09 00:16:25 +0900

    update

--- UART/uart.v
+++ UART/uart.v
@@ -1,17 +1,22 @@
-module uart(CLK50M, BUTTON, RX, TX, state);
+module uart(CLK50M, RX, TX, b_state, r_state, s_state);
     input CLK50M;
-    input BUTTON;
     input RX;
 	output TX;
-	output [1:0] state;
+	output b_state, r_state, s_state;
 
 	wire clk;
-
+	
+	 reg TX = 1'b1;
+	 reg b_state = 1'b0;
+	 reg r_state = 1'b0;
+	 reg s_state = 1'b0;
+	
+	 reg [1:0] state = 2'b00;
     reg [7:0] rx_data = 8'b00000000;
     reg [2:0] rx_index = 3'b000;
 
     reg [7:0] tx_data = 8'b00000000;
-    reg [2:0] rx_index = 3'b000;
+    reg [2:0] tx_index = 3'b000;
 		
     clk_gen clk_gen(CLK50M, clk);
 	 
@@ -21,12 +26,19 @@ module uart(CLK50M, BUTTON, RX, TX, state);
     always @(posedge clk) begin
         if(state == 0) begin            //busy
             TX <= 1'b1;
+				b_state <= 1'b1;
+				r_state <= 1'b0;
+				s_state <= 1'b0;
             if(RX == 1'b0) state <= 1;  //if(getting data begin) busy -> receve
         end
 
         if(state == 1) begin            //receve
-                rx_data[rx_index] <= RX;
-            if(rx_index == 8) begin 
+				b_state <= 1'b0;
+				r_state <= 1'b1;
+				s_state <= 1'b0;
+				
+            tx_data[rx_index] <= RX;
+            if(rx_index == 7) begin 
                 rx_index <= 0;
                 state <= 2;             //if(getting data end) receve -> send
                 TX <= 1'b0;             //send start bit
@@ -36,11 +48,14 @@ module uart(CLK50M, BUTTON, RX, TX, state);
         end
 
         if(state == 2) begin            //send
+				b_state <= 1'b0;
+				r_state <= 1'b0;
+				s_state <= 1'b1;
             TX <= tx_data[tx_index];
-            if(tx_index == 8) begin
+            if(tx_index == 7) begin
                 tx_index <= 0;
                 state <= 0;             //if(sending data end) send -> busy
-                TX <= 1'b1;             //send end bit
+                //TX <= 1'b1;             //send end bit
             end else begin
                 tx_index <= tx_index + 1;
             end
