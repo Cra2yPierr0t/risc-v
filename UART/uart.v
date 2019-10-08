4ca50ea4bc9e8853b38c9c549cc6c4a5df8c5c87
pirrot <uchiyama.kazuhide@gmail.com>
2019-10-08 22:07:13 +0900

    update

--- UART/uart.v
+++ UART/uart.v
@@ -1,28 +1,49 @@
-module uart(CLK50M, BUTTON, TX, RX, state);
+module uart(CLK50M, BUTTON, RX, TX, state);
     input CLK50M;
     input BUTTON;
-    input TX;
-	output RX;
+    input RX;
+	output TX;
 	output [1:0] state;
-	 
+
 	wire clk;
+
+    reg [7:0] rx_data = 8'b00000000;
+    reg [2:0] rx_index = 3'b000;
+
+    reg [7:0] tx_data = 8'b00000000;
+    reg [2:0] rx_index = 3'b000;
 		
-	reg RX = 1;
-    reg [9:0] tx_data = 10'h3ff;
-    reg [0:9] rx_data = 10'b0001100011;
-    reg [3:0] bit_index = 4'b0;
-    reg [1:0] state = 0;
-	reg button = 0;
-	 
     clk_gen clk_gen(CLK50M, clk);
 	 
     //busy -> receve -> send -> busy
     //busy -> send -> busy
 
     always @(posedge clk) begin
-        if(state == 0) begin //busy
+        if(state == 0) begin            //busy
+            TX <= 1'b1;
+            if(RX == 1'b0) state <= 1;  //if(getting data begin) busy -> receve
+        end
 
-        if(state == 1) begin //receve
-        if(state == 2) begin //send
+        if(state == 1) begin            //receve
+                rx_data[rx_index] <= RX;
+            if(rx_index == 8) begin 
+                rx_index <= 0;
+                state <= 2;             //if(getting data end) receve -> send
+                TX <= 1'b0;             //send start bit
+            end else begin 
+                rx_index <= rx_index + 1;
+            end
+        end
+
+        if(state == 2) begin            //send
+            TX <= tx_data[tx_index];
+            if(tx_index == 8) begin
+                tx_index <= 0;
+                state <= 0;             //if(sending data end) send -> busy
+                TX <= 1'b1;             //send end bit
+            end else begin
+                tx_index <= tx_index + 1;
+            end
+        end
     end
 endmodule
